6118d99cc8a9ef99e97752df4afc0a97934f915c
lemercier <jeanmarie.lemercier@uni-hamburg.de>
2024-03-06 10:37:13 +0100

    s

---
+++ eval.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pc=sppc1
+
+if [ "$pc" = sppc1 ]; then
+    data_dir=/data/lemercier/databases
+    home_dir=/export/home/lemercier
+elif [ "$pc" = spgpu1 ]; then
+    data_dir=/data/lemercier/databases
+    home_dir=/data1/lemercier
+elif [ "$pc" = spgpu2 ]; then
+    data_dir=/data3/lemercier/databases
+    home_dir=/export/home/lemercier
+fi;
+
+python enhancement.py  \
+    --test_dir $data_dir/VCTK-Reverb/test/reverberant \
+    --enhanced_dir exp/vctk-reverb_denoiser_ncsnppm \
+    --ckpt /export/home/lemercier/code/_public_repos/storm/.logs/denoiser_ncsnpp_vctk-reverb_epoch=89.ckpt \
+    --mode denoiser-only \
+    # --n_files 100
+
+python enhancement.py  \
+    --test_dir $data_dir/VCTK-Reverb/test/reverberant \
+    --enhanced_dir exp/vctk-reverb_sgmse+_ncsnppm \
+    --ckpt /export/home/lemercier/code/_public_repos/storm/.logs/sgmse+_ncsnpp_vctk-reverb_epoch=140.ckpt \
+    --mode score-only \
+    --N 30
+    # --n_files 100
+
