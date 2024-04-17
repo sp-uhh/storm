7cca3418ed4fc9ede3bb1a86a9a0d028e8654a60
lemercier <lemercier@informatik.uni-hamburg.de>
2024-04-17 15:21:29 +0200

    ready scripts for hpc training

--- train_hpc.sh
+++ train_hpc.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=4 # number of GPUs
 #SBATCH --cpus-per-task=4 # number of CPUs per GUP
 #SBATCH --partition=a100
-#SBATCH --job-name=storm_vctk-reverb
+#SBATCH --job-name=ncsnpp_vctk-reverb
 #SBATCH --output=.slurm/%x-%j.out
 #SBATCH --error=.slurm/%x-%j.err
 #SBATCH --time=23:59:00
@@ -17,6 +17,24 @@ module load python cuda/12.1 gcc/10 ninja
 export HTTP_PROXY=http://proxy:80
 export HTTPS_PROXY=http://proxy:80
 
+srun python $HOME/code/storm/train.py \
+    --base_dir $HPCVAULT/databases/VCTK-Reverb.h5 \
+    --mode denoiser-only \
+    --format reverb_vctk \
+    --num_frames 512 \
+    --batch_size 4 \
+    --backbone_denoiser ncsnpp \
+    --devices 4
+
+# srun python $HOME/code/storm/train.py \
+#     --base_dir $HPCVAULT/databases/VCTK-Reverb.h5 \
+#     --mode score-only \
+#     --format reverb_vctk \
+#     --num_frames 512 \
+#     --batch_size 4 \
+#     --backbone_score ncsnpp \
+#     --devices 4
+
 # srun python $HOME/code/storm/train.py \
 #     --base_dir $HPCVAULT/databases/VCTK-Reverb.h5 \
 #     --mode regen-joint-training \
@@ -24,20 +42,9 @@ export HTTPS_PROXY=http://proxy:80
 #     --num_frames 512 \
 #     --batch_size 4 \
 #     --backbone_denoiser ncsnpp \
-#     --pretrained_denoiser $HOME/code/storm/.logs/ncsnppm_denoiser_epoch=89.ckpt \
+#     --pretrained_denoiser $HOME/code/storm/.logs/ncsnppm_denoiser_epoch=XXX.ckpt \
 #     --backbone_score ncsnpp \
 #     --condition both \
 #     --devices 4
 
-srun python $HOME/code/storm/train.py \
-    --base_dir $HPCVAULT/databases/VCTK-Reverb.h5 \
-    --mode regen-joint-training \
-    --format reverb_vctk \
-    --num_frames 512 \
-    --batch_size 4 \
-    --backbone_denoiser ncsnpp \
-    --pretrained_denoiser $HOME/code/storm/.logs/ncsnppm_denoiser_epoch=89.ckpt \
-    --backbone_score ncsnpp \
-    --condition both \
-    --devices 4 \
-    --resume_from_checkpoint $HOME/code/storm/.logs/mode=regen-joint-training_sde=OUVESDE_score=ncsnpp_denoiser=ncsnpp_condition=both_data=reverb_vctk_ch=1/version_7/checkpoints/last.ckpt
+#     --resume_from_checkpoint $HOME/code/storm/.logs/mode=regen-joint-training_sde=OUVESDE_score=ncsnpp_denoiser=ncsnpp_condition=both_data=reverb_vctk_ch=1/version_7/checkpoints/last.ckpt
